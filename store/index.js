7ffc504a25c89e01b903f443cc670e10340cebde
Luckyman909 <mingw2816@gmail.com>
2021-04-15 04:18:34 +0530

    test1

---
+++ store/index.js
@@ -0,0 +1,16 @@
+export const state = () => ({
+    announcement: null
+  })
+  
+  export const mutations = {
+    SET_USER(state, user){
+      state.user = user
+    }
+  }
+  
+  export const actions = {
+    async nuxtServerInit({ commit }, { $strapi }) {
+      const user = await $strapi.fetchUser()
+    commit('SET_USER', user)
+  }
+}
