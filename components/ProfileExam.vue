76323031037a0e09566738f72caaae9e05e53f4b
Luckyman909 <mingw2816@gmail.com>
2021-04-15 09:56:08 +0530

    fixing

--- components/ProfileExam.vue
+++ components/ProfileExam.vue
@@ -1,19 +1,52 @@
 <template>
-    <div>
-        <ClientOnly>
-        <div v-if="$strapi.user">
-            Hello loged in!
+<div>
+    <LoginHome v-if="showModalS" @closeSmodal='showModalS=false'></LoginHome>
+    <div v-else class="parent">
+        <div class="child">
+            <div v-if="!logedin">
+               <h4> Hay There, Stranger !</h4>
+               <button class="main-btn" @click="showModalS=true">Sign In</button>
+            </div>
+            <div v-else>
+                <div v-if="isResultAvilable" class="profilepage">
+                    <h4>congratulations ! {{user}} <br> Position : {{ position }} </h4>
+                    <!-- <h4>{{}} Days {{}} Hours {{}} </h4> -->
+                    <button class="main-btn">Get Certificate!</button>
+                    <button class="main-btn">Result</button>
+                </div>
+                <div v-else>
+                    <h4>Welcome ! {{username}}</h4><br>
+                    <button class="main-btn" v-if="isExamAvilable">Start Exam !</button>
+                    <div v-else>
+                        <h3>The Exam Will Start In</h3>
+                        <countdown-exam @finished="isExamAvilable=true"  class="main-btn" style="cursor: not-allowed;" end="Apr 16 2021 15:00:00" :onlyminutes="false"></countdown-exam>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div v-else>
-            lOGEDED IN FUNCTION
-        </div> 
-        </ClientOnly>
     </div>
-
+    </div>
 </template>
 
 <script>
+import LoginHome from '../components/LoginHome'
+import CountdownExam from './CountdownExam.vue'
+import DataLoad from './DataLoad.vue'
 export default {
+    components:{
+        CountdownExam,
+        LoginHome,
+        DataLoad,
+        
+    },
+    data(){
+        return{
+            isExamAvilable:false,
+            isResultAvilable:false,
+            position:'',
+            showModalS:false,
+        }
+    },
     computed:{
         username({$strapi}){
             console.log("from profile",$strapi.user)
